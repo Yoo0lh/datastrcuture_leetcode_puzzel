6f55fd849391c3d0583f4cee478d2d6033bc354e
yoo <>
2021-09-18 21:47:49 -0400

    algorithme

--- linked_list/pop.c
+++ linked_list/pop.c
@@ -1,24 +1,40 @@
-#include <stdio.h>  
+/*******************************************************************
+*   pop operation in alinked list (delelt the head of linked list) 
+*   exaple :
+*           input : 1-->2-->3-->4-->NULL
+*           output: pop valu is 1
+*                   2-->3-->4-->NULL
+*   time complexity of the pop operation is o(1)
+*************************************************************************/
+
+#include <stdio.h>
 #include <stdlib.h>
 #include "linked.h"
-int pop(struct node **elem)
+int pop(struct node **item)
 {
-    if(*elem  == NULL)
+    struct node *tmp;
+    if(*item == NULL)
         return (-1);
-    struct node *p = *elem;
-    int res = p->value;
-    (*elem) = (*elem)->next;
-    free(p);
-    return(res);
+    tmp = *item;
+    int res = tmp->value;
+    *item = (*item)->next;
+    free (tmp);
+    return (res);
 }
 int main()
 {
-    struct node *elem = NULL;
     int c[] = {1,2,3,4};
+    struct node *item = NULL;
     int len = sizeof(c)/sizeof(c[0]);
-    for(int i = 0; i < len; i++)
-        insert(&elem, c[i]);
-    int res = pop(&elem);
-    printf("the pop %d\n", res);
+    for(int i = len - 1 ; i >= 0; i--)
+        insert(&item, c[i]);
+    
+    printf("pop value is %d \n", pop(&item));
+    while(item != NULL)
+    {
+        printf("%d-->", item->value);
+        item = item->next;
+    }
+    printf("NULL");
     return (0);
 }
