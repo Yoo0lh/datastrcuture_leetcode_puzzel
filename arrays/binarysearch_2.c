77e9303d940ea5f5c736df43d2016ff8ddbd59c8
yoo <>
2021-10-04 01:08:24 -0400

    datastruct

---
+++ arrays/binarysearch_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int binara_search(int *s, int len, int find)
+{
+    int hl;
+    int i = 0;
+    int e = len - 1;
+
+    while(i <= e)
+    {
+        int hl = (i + e)/2;
+        if (s[hl] == find)
+            return hl;
+        else if (s[hl] > find)
+            e = hl - 1;
+        else 
+            i = hl + 1;
+    }
+    return -1;
+}
+int main()
+{
+    int c[] = {5,10,15,20,40,50,100};
+    int len = sizeof(c)/sizeof(c[0]);
+    printf("res ---> %d ", binara_search(c, len, 5));
+    return 0;
+}
